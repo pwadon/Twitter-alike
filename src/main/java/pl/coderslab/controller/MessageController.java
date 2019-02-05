557eb5a5113db0301272d2dd8aaddfd4993118dc
Przemyslaw Wadon <p.wadon@gazeta.pl>
2019-02-05 10:51:31 +0100

    git ignore edit

--- src/main/java/pl/coderslab/controller/MessageController.java
+++ src/main/java/pl/coderslab/controller/MessageController.java
@@ -3,17 +3,17 @@ package pl.coderslab.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import pl.coderslab.entity.Message;
 import pl.coderslab.entity.User;
 import pl.coderslab.repository.MessageRepository;
+import pl.coderslab.repository.UserRepository;
 
 import javax.jws.soap.SOAPBinding;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -22,6 +22,9 @@ public class MessageController {
 
     @Autowired
     private MessageRepository messageRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @RequestMapping("/userMessages")
     public String messagesAdd(Model model, HttpSession session){
@@ -47,6 +50,21 @@ public class MessageController {
         return "messages/userMessages";
     }
 
+    @GetMapping("/sendMessage")
+    public String sendMessage(Model model, HttpServletRequest request){
+        model.addAttribute("message", new Message());
+        model.addAttribute("formAction", request.getContextPath() +"/message/sendMessage");
+        return "messages/sendMessage";
+    }
+    @PostMapping("/sendMessage")
+    public String sendMessage(@Valid Message message, BindingResult errors, HttpServletRequest request){
+        if (errors.hasErrors()){
+            return "messages/sendMessage";
+        }
+        messageRepository.save(message);
+        return "redirect:" + request.getContextPath()+"/home";
+    }
+
     @GetMapping("/showMsg/{id}")
     public String showMsg(Model model, @PathVariable Long id, HttpSession session) {
         Message message = messageRepository.findOne(id);
@@ -57,5 +75,9 @@ public class MessageController {
         messageRepository.save(message);
         model.addAttribute("msg", message);
         return "messages/message";
+    }
+    @ModelAttribute("users")
+    public List<User> users(){
+        return userRepository.findAll();
     }
 }
--- src/main/webapp/WEB-INF/views/messages/sendMessage.jsp
+++ src/main/webapp/WEB-INF/views/messages/sendMessage.jsp
@@ -1,16 +1,37 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: paladyn
-  Date: 27.01.19
-  Time: 10:19
-  To change this template use File | Settings | File Templates.
---%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>Send Message</title>
 </head>
 <body>
+<form:form method="post"
+           action="${formAction}"
+           modelAttribute="message"
+           cssClass="container col-6" >
 
+
+    <form:hidden path="id" />
+
+    <form:textarea path="msg" placeholder="message" cssClass="form-input" />
+    <form:errors path="msg" cssClass="alet alert-danger" element="div"/>
+
+    <form:hidden path="readed"/>
+    <form:input path="title" placeholder="title" cssClass="form-input"/>
+    <form:errors path="title" cssClass="alet alert-danger" element="div"/>
+
+   <form:select path="receiver">
+
+       <form:options items="${users}" itemValue="id" itemLabel="username"/>
+
+   </form:select>
+
+    <form:hidden path="sender" value="${user.getId()}"/>
+
+
+    <input type="submit"  class="btn btn-success">
+</form:form>
 </body>
 </html>
